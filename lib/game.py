89170c6b115a3f13eeb02fe1be06054a171f2cec
Matt Todd <matt.c.todd@gmail.com>
2015-08-10 13:05:43 -0500

    got some basic interation going

--- lib/game.py
+++ lib/game.py
@@ -1,9 +1,16 @@
 import console
+import player_input
 
 class Game():
-   def play(self):
-      console.display("Let's play a game!")
+   @classmethod
+   def play(cls):
+      console.display("Rock (r), Paper (p) or Scissors (s):")
+      choice = player_input.read_choice()
+      console.display("You played " + cls._translate_choice(choice) + "!")
 
+   @classmethod
+   def _translate_choice(cls, choice):
+      return "Rock" if choice == "r" else "Scissors"
 
 if __name__ == "__main__":
-   Game().play()
+   Game.play()
--- tests/game_test.py
+++ tests/game_test.py
@@ -1,10 +1,26 @@
 from lib.game import Game
 import lib.console
 
-def test_play(mocker):
+def test_play_prompts_for_users_option(mocker):
    mocker.patch('lib.console.display')
+   mocker.patch('lib.player_input.read_choice')
 
-   Game().play()
+   Game.play()
 
-   lib.console.display.assert_called_once_with("Let's play a game!")
+   lib.console.display.assert_any_call("Rock (r), Paper (p) or Scissors (s):")
 
+def test_play_players_choice_is_displayed(mocker):
+   mocker.patch('lib.console.display')
+   mocker.patch('lib.player_input.read_choice', return_value="r")
+
+   Game.play()
+
+   lib.console.display.assert_any_call("You played Rock!")
+
+def test_play_players_choice_is_displayed_if_user_picks_other_option(mocker):
+   mocker.patch('lib.console.display')
+   mocker.patch('lib.player_input.read_choice', return_value="s")
+
+   Game.play()
+
+   lib.console.display.assert_any_call("You played Scissors!")
