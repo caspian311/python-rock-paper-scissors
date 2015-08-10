31bfd26a878f0671737a5e0a6b7c4ce629c699e0
Matt Todd <matt.c.todd@gmail.com>
2015-08-10 13:56:54 -0500

    display the computers play

--- lib/game.py
+++ lib/game.py
@@ -1,16 +1,21 @@
 import console
 import player_input
+import computer_input
 
 class Game():
    @classmethod
    def play(cls):
       console.display("Rock (r), Paper (p) or Scissors (s):")
-      choice = player_input.read_choice()
-      console.display("You played " + cls._translate_choice(choice) + "!")
+
+      player_choice = player_input.read_choice()
+      computer_choice = computer_input.read_choice()
+
+      console.display("You played " + cls._translate_choice(player_choice) + "!")
+      console.display("Computer played " + cls._translate_choice(computer_choice) + "!")
 
    @classmethod
    def _translate_choice(cls, choice):
-      return "Rock" if choice == "r" else "Scissors"
+      return "Rock" if choice == "r" else "Scissors" if choice == "s" else "Paper"
 
 if __name__ == "__main__":
    Game.play()
--- tests/game_test.py
+++ tests/game_test.py
@@ -4,6 +4,7 @@ import lib.console
 def test_play_prompts_for_users_option(mocker):
    mocker.patch('lib.console.display')
    mocker.patch('lib.player_input.read_choice')
+   mocker.patch('lib.computer_input.read_choice')
 
    Game.play()
 
@@ -11,6 +12,7 @@ def test_play_prompts_for_users_option(mocker):
 
 def test_play_players_choice_is_displayed(mocker):
    mocker.patch('lib.console.display')
+   mocker.patch('lib.computer_input.read_choice')
    mocker.patch('lib.player_input.read_choice', return_value="r")
 
    Game.play()
@@ -19,8 +21,28 @@ def test_play_players_choice_is_displayed(mocker):
 
 def test_play_players_choice_is_displayed_if_user_picks_other_option(mocker):
    mocker.patch('lib.console.display')
+   mocker.patch('lib.computer_input.read_choice')
    mocker.patch('lib.player_input.read_choice', return_value="s")
 
    Game.play()
 
    lib.console.display.assert_any_call("You played Scissors!")
+
+def test_play_computers_choice_is_displayed(mocker):
+   mocker.patch('lib.console.display')
+   mocker.patch('lib.player_input.read_choice')
+   mocker.patch('lib.computer_input.read_choice', return_value="p")
+
+   Game.play()
+
+   lib.console.display.assert_any_call("Computer played Paper!")
+
+def test_play_computers_choice_is_displayed_if_computer_picks_other_option(mocker):
+   mocker.patch('lib.console.display')
+   mocker.patch('lib.player_input.read_choice')
+   mocker.patch('lib.computer_input.read_choice', return_value="r")
+
+   Game.play()
+
+   lib.console.display.assert_any_call("Computer played Rock!")
+
